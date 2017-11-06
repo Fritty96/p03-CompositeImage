d5fc5030eac26d7fffc3582b718edb6d3667733d
Fritty96 <31226164+Fritty96@users.noreply.github.com>
2017-11-06 08:39:29 -0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include “bitmap.h”
+#include <vector>
+
+Using namespace std;
+
+//Receives bitmap filename, check if file is valid. Print error message if invalid.
+int filevalid(bitmap &, );
+
+int main(){
+
+
+
+return 0;
+}
+
+
+
+//Ask user for Filename
+//check if filename valid.
+//if invalid, print error message
+//Ask again if under 10
+//check files against one another
+//if wrong size, disregard file
+//Continue asking for file until user says ‘done’, or until 10
+//If user says done, with two images of different sizes, print error message
+//if user says done with one image, print error message
+//once count is full or user says done with valid images,
+//create vector of vectors of the images and change image colors using
+//averages of each rbg value 
+//combine files after rgb change
+//print progress after each file is processed
+//save complete combination
