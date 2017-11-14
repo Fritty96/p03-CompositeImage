faf4055f2daf2e94b24726a5a23accb00df8dcab
Fritty96 <mphillips39@mail.csuchico.edu>
2017-11-13 20:23:28 -0800

    More

--- main.cpp
+++ main.cpp
@@ -44,8 +44,12 @@ makeAverage();
 return 0;
 }
 
-
-
+//Takes entered string, checks if it's bmp- prints error if not.
+//If bmp, goes on to check if its the first string
+//if yes, puts it in vector
+//if no, compares the string against the first string for size
+//if same size, adds it to the vector of strings
+//if not the same size, prints error message and discards
 
 
 void filevalid(string a, vector < string > & bmp, int & i){
@@ -84,31 +88,45 @@ valid = image.isImage();
 
 }
 
-
-//average function
+//supposed to go through all of the user input pictures
+//find the sum of each pixel and average
+//place new average within a new vector to create composite image
 
 vector <vector <Pixel > > makeAverage(vector < vector <vector <Pixel> > > huge ){
-
-vector < vector < vector < Pixel > > > huge;
-vector <vector <Pixel > > result;
 Pixel rgb;
- sumR = 0;
- sumG = 0;
- sumB = 0;
+vector <vector <Pixel > > result;
+
+
+
+
             
 
 
     for (int row = 0; row < huge.size(); row++){
         for (int col = 0; col < huge[row].size(); col++){
+        
+        int sumR = 0;
+        int sumG = 0;
+        int sumB = 0;
             for (int depth = 0; depth < huge[row][col].size(); depth++){
+            
             rgb = huge[row][col][depth];
             sumR = sumR + rgb.red;
             sumG = sumG + rgb.green;
             sumB = sumB + rbg.blue;
             
             }
-            int average = (sumR + sumG + sumB)/depth.size();
-
+        int averageR = sumR /depth;
+        int averageG = sumG /depth;
+        int averageB = sumB /depth;
+        rgb.red = averageR;
+        rgb.green = averageG;
+        rgb.blue = averageB;
+      
+       result[row][col] = rgb;   
        }
    }
+ 
+
+
 }
